7d8a8ebc080e8450fb3cef6c68c174f5066c89e0
韩天乐 <tianle_han@163.com>
2023-02-28 00:16:41 +0800

    format code with prettier

--- pdfparser.js
+++ pdfparser.js
@@ -1,183 +1,194 @@
-'use strict';
+"use strict";
 
-let fs = require('fs'),
-	stream = require('stream'),
-	nodeUtil = require("util"),
-    _ = require("lodash"),
-    async = require("async"),
-	PDFJS = require("./lib/pdf.js");
+let fs = require("fs"),
+  stream = require("stream"),
+  nodeUtil = require("util"),
+  _ = require("lodash"),
+  async = require("async"),
+  PDFJS = require("./lib/pdf.js");
 
 let PDFParser = (function () {
-    // private static
-    let _nextId = 1;
-    let _name = 'PDFParser';
+  // private static
+  let _nextId = 1;
+  let _name = "PDFParser";
 
-    let _binBuffer = {};
-    let _maxBinBufferCount = 10;
+  let _binBuffer = {};
+  let _maxBinBufferCount = 10;
 
-	//private methods, needs to invoked by [funcName].call(this, ...)
-	let _onPDFJSParseDataReady = function(data) {
-		if (!data) { //v1.1.2: data===null means end of parsed data
-			nodeUtil.p2jinfo("PDF parsing completed.");
-			let output = {"formImage": this.data};
-			this.emit("pdfParser_dataReady", output);
-			if (typeof this.flushCallback === 'function') {
-				this.push(output);
-				this.flushCallback();
-				this.flushCallback = null;
-			}
-		}
-		else {
-			Object.assign(this.data, data);
-		}
-	};
+  //private methods, needs to invoked by [funcName].call(this, ...)
+  let _onPDFJSParseDataReady = function (data) {
+    if (!data) {
+      //v1.1.2: data===null means end of parsed data
+      nodeUtil.p2jinfo("PDF parsing completed.");
+      let output = { formImage: this.data };
+      this.emit("pdfParser_dataReady", output);
+      if (typeof this.flushCallback === "function") {
+        this.push(output);
+        this.flushCallback();
+        this.flushCallback = null;
+      }
+    } else {
+      Object.assign(this.data, data);
+    }
+  };
 
-	let _onPDFJSParserDataError = function(data) {
-		this.data = null;
-		this.emit("pdfParser_dataError", {"parserError": data});
-	};
+  let _onPDFJSParserDataError = function (data) {
+    this.data = null;
+    this.emit("pdfParser_dataError", { parserError: data });
+  };
 
-	let _startParsingPDF = function(buffer) {
-		this.data = {};
+  let _startParsingPDF = function (buffer) {
+    this.data = {};
 
-		this.PDFJS.on("pdfjs_parseDataReady", _onPDFJSParseDataReady.bind(this));
-		this.PDFJS.on("pdfjs_parseDataError", _onPDFJSParserDataError.bind(this));
+    this.PDFJS.on("pdfjs_parseDataReady", _onPDFJSParseDataReady.bind(this));
+    this.PDFJS.on("pdfjs_parseDataError", _onPDFJSParserDataError.bind(this));
 
-		this.PDFJS.parsePDFData(buffer || _binBuffer[this.pdfFilePath]);
-	};
+    this.PDFJS.parsePDFData(buffer || _binBuffer[this.pdfFilePath]);
+  };
 
-	let _processBinaryCache = function() {
-		if (_.has(_binBuffer, this.pdfFilePath)) {
-			_startParsingPDF.call(this);
-			return true;
-		}
+  let _processBinaryCache = function () {
+    if (_.has(_binBuffer, this.pdfFilePath)) {
+      _startParsingPDF.call(this);
+      return true;
+    }
 
-		let allKeys = _.keys(_binBuffer);
-		if (allKeys.length > _maxBinBufferCount) {
-			let idx = this.get_id() % _maxBinBufferCount;
-			let key = allKeys[idx];
-			_binBuffer[key] = null;
-			delete _binBuffer[key];
+    let allKeys = _.keys(_binBuffer);
+    if (allKeys.length > _maxBinBufferCount) {
+      let idx = this.get_id() % _maxBinBufferCount;
+      let key = allKeys[idx];
+      _binBuffer[key] = null;
+      delete _binBuffer[key];
 
-			nodeUtil.p2jinfo("re-cycled cache for " + key);
-		}
+      nodeUtil.p2jinfo("re-cycled cache for " + key);
+    }
 
-		return false;
-	};
+    return false;
+  };
 
-	let _processPDFContent = function(err, data) {
-		nodeUtil.p2jinfo("Load PDF file status:" + (!!err ? "Error!" : "Success!") );
-		if (err) {
-			this.data = err;
-			this.emit("pdfParser_dataError", this);
-		}
-		else {
-			_binBuffer[this.pdfFilePath] = data;
-			_startParsingPDF.call(this);
-		}
-	};
+  let _processPDFContent = function (err, data) {
+    nodeUtil.p2jinfo("Load PDF file status:" + (!!err ? "Error!" : "Success!"));
+    if (err) {
+      this.data = err;
+      this.emit("pdfParser_dataError", this);
+    } else {
+      _binBuffer[this.pdfFilePath] = data;
+      _startParsingPDF.call(this);
+    }
+  };
 
-	let _createContentStream = function(jsonObj) {
-		let rStream = new stream.Readable({objectMode: true});
-		rStream.push(jsonObj);
-		rStream.push(null);
-		return rStream;
-	};
+  let _createContentStream = function (jsonObj) {
+    let rStream = new stream.Readable({ objectMode: true });
+    rStream.push(jsonObj);
+    rStream.push(null);
+    return rStream;
+  };
 
-	// constructor
-    function PdfParser(context, needRawText) {
-		//call constructor for super class
-	    stream.Transform.call(this, {objectMode: true, bufferSize: 64 * 1024});
-	
-        // private
-        let _id = _nextId++;
+  // constructor
+  function PdfParser(context, needRawText) {
+    //call constructor for super class
+    stream.Transform.call(this, { objectMode: true, bufferSize: 64 * 1024 });
 
-        // public (every instance will have their own copy of these methods, needs to be lightweight)
-        this.get_id = () => _id;
-        this.get_name = () => _name + _id;
+    // private
+    let _id = _nextId++;
 
-        // service context object, only used in Web Service project; null in command line
-        this.context = context;
+    // public (every instance will have their own copy of these methods, needs to be lightweight)
+    this.get_id = () => _id;
+    this.get_name = () => _name + _id;
 
-        this.pdfFilePath = null; //current PDF file to load and parse, null means loading/parsing not started
-        this.data = null; //if file read success, data is PDF content; if failed, data is "err" object
-        this.PDFJS = new PDFJS(needRawText);
-        this.processFieldInfoXML = false;//disable additional _fieldInfo.xml parsing and merging
+    // service context object, only used in Web Service project; null in command line
+    this.context = context;
 
-	    this.chunks = [];
-	    this.flushCallback = null;
-	}
-	// inherit from event emitter
-	nodeUtil.inherits(PdfParser, stream.Transform);
+    this.pdfFilePath = null; //current PDF file to load and parse, null means loading/parsing not started
+    this.data = null; //if file read success, data is PDF content; if failed, data is "err" object
+    this.PDFJS = new PDFJS(needRawText);
+    this.processFieldInfoXML = false; //disable additional _fieldInfo.xml parsing and merging
 
-	//implements transform stream
-	PdfParser.prototype._transform = function (chunk, enc, callback) {
-		this.chunks.push(Buffer.isBuffer(chunk) ? chunk : new Buffer(chunk, enc));
-		callback();
-	};
+    this.chunks = [];
+    this.flushCallback = null;
+  }
 
-	PdfParser.prototype._flush = function(callback) {
-		this.flushCallback = callback;
-		this.parseBuffer(Buffer.concat(this.chunks));
-	};
+  // inherit from event emitter
+  nodeUtil.inherits(PdfParser, stream.Transform);
 
-	PdfParser.prototype.fq = async.queue( (task, callback) => {
-		fs.readFile(task.path, callback);
-	}, 100);
+  //implements transform stream
+  PdfParser.prototype._transform = function (chunk, enc, callback) {
+    this.chunks.push(Buffer.isBuffer(chunk) ? chunk : new Buffer(chunk, enc));
+    callback();
+  };
 
-	//public APIs
-	PdfParser.prototype.setVerbosity = function(verbosity) {
-		nodeUtil.verbosity(verbosity || 0);
-	};
+  PdfParser.prototype._flush = function (callback) {
+    this.flushCallback = callback;
+    this.parseBuffer(Buffer.concat(this.chunks));
+  };
 
-	PdfParser.prototype.loadPDF = function(pdfFilePath, verbosity) {
-		this.setVerbosity(verbosity);
-		nodeUtil.p2jinfo("about to load PDF file " + pdfFilePath);
+  // fq is a queue which can process 100 concurrently
+  PdfParser.prototype.fq = async.queue((task, callback) => {
+    fs.readFile(task.path, callback);
+  }, 100);
 
-		this.pdfFilePath = pdfFilePath;
-		if (this.processFieldInfoXML) {
-			this.PDFJS.tryLoadFieldInfoXML(pdfFilePath);
-		}
+  //public APIs
+  PdfParser.prototype.setVerbosity = function (verbosity) {
+    nodeUtil.verbosity(verbosity || 0);
+  };
 
-		if (_processBinaryCache.call(this))
-			return;
+  PdfParser.prototype.loadPDF = function (pdfFilePath, verbosity) {
+    this.setVerbosity(verbosity);
+    nodeUtil.p2jinfo("about to load PDF file " + pdfFilePath);
 
-		this.fq.push({path: pdfFilePath}, _processPDFContent.bind(this));
-	};
+    this.pdfFilePath = pdfFilePath;
+    if (this.processFieldInfoXML) {
+      this.PDFJS.tryLoadFieldInfoXML(pdfFilePath);
+    }
 
-	// Introduce a way to directly process buffers without the need to write it to a temporary file
-	PdfParser.prototype.parseBuffer = function(pdfBuffer) {
-		_startParsingPDF.call(this, pdfBuffer);
-	};
+    if (_processBinaryCache.call(this)) return;
+    // callback function of _processPDFContent.bind(this) means this._processPDFContent()
+    this.fq.push({ path: pdfFilePath }, _processPDFContent.bind(this));
+  };
 
-	PdfParser.prototype.getRawTextContent = function() { return this.PDFJS.getRawTextContent(); };
-	PdfParser.prototype.getRawTextContentStream = function() { return _createContentStream(this.getRawTextContent()); };
+  // Introduce a way to directly process buffers without the need to write it to a temporary file
+  PdfParser.prototype.parseBuffer = function (pdfBuffer) {
+    _startParsingPDF.call(this, pdfBuffer);
+  };
 
-	PdfParser.prototype.getAllFieldsTypes = function() { return this.PDFJS.getAllFieldsTypes(); };
-	PdfParser.prototype.getAllFieldsTypesStream = function() { return _createContentStream(this.getAllFieldsTypes()); };
+  PdfParser.prototype.getRawTextContent = function () {
+    return this.PDFJS.getRawTextContent();
+  };
+  PdfParser.prototype.getRawTextContentStream = function () {
+    return _createContentStream(this.getRawTextContent());
+  };
 
-	PdfParser.prototype.getMergedTextBlocksIfNeeded = function() { return {"formImage": this.PDFJS.getMergedTextBlocksIfNeeded()}; };
-	PdfParser.prototype.getMergedTextBlocksStream = function() { return _createContentStream(this.getMergedTextBlocksIfNeeded()); };
+  PdfParser.prototype.getAllFieldsTypes = function () {
+    return this.PDFJS.getAllFieldsTypes();
+  };
+  PdfParser.prototype.getAllFieldsTypesStream = function () {
+    return _createContentStream(this.getAllFieldsTypes());
+  };
 
-	PdfParser.prototype.destroy = function() {
-		this.removeAllListeners();
+  PdfParser.prototype.getMergedTextBlocksIfNeeded = function () {
+    return { formImage: this.PDFJS.getMergedTextBlocksIfNeeded() };
+  };
+  PdfParser.prototype.getMergedTextBlocksStream = function () {
+    return _createContentStream(this.getMergedTextBlocksIfNeeded());
+  };
 
-		//context object will be set in Web Service project, but not in command line utility
-		if (this.context) {
-			this.context.destroy();
-			this.context = null;
-		}
+  PdfParser.prototype.destroy = function () {
+    this.removeAllListeners();
 
-		this.pdfFilePath = null;
-		this.data = null;
-		this.chunks = null;
+    //context object will be set in Web Service project, but not in command line utility
+    if (this.context) {
+      this.context.destroy();
+      this.context = null;
+    }
 
-		this.PDFJS.destroy();
-		this.PDFJS = null;
-	};
+    this.pdfFilePath = null;
+    this.data = null;
+    this.chunks = null;
 
-	return PdfParser;
+    this.PDFJS.destroy();
+    this.PDFJS = null;
+  };
+
+  return PdfParser;
 })();
 
 module.exports = PDFParser;
-
